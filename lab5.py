cdb37ff0845bb7903445bf514200b4d4c212d4bc
unknown <danyakononov2004@gmail.com>
2024-12-25 22:46:08 +0700

    Авторизация

--- lab5.py
+++ lab5.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template, request, redirect, session
 import psycopg2
+from psycopg2.extras import RealDictCursor
 lab5 = Blueprint("lab5", __name__)
 
 @lab5.route('/lab5/')
 def labor5():
-    return render_template('lab5/lab5.html')
+    return render_template('lab5/lab5.html',login=session.get('login'))
 
 @lab5.route('/lab5/register', methods = ['GET', 'POST'])
 def register():
@@ -20,7 +21,7 @@ def register():
     database = 'danil_kononov_knowledge_base',
     user = 'danil_kononov_knowledge_base',
     password = '1234'
-)
+		)
     cur = conn.cursor()
 
     cur.execute(f"SELECT login FROM users WHERE login='{login}';")
@@ -33,4 +34,37 @@ def register():
     cur.close()
     conn.close()
     return render_template('lab5/success.html', login=login)
+
+@lab5.route('/lab5/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab5/login.html')
+    login = request.form.get('login')
+    password = request.form.get('password')
+    if not (login or password):
+        return render_template('lab5/login.html', error="Заполните поля")
+    conn = psycopg2.connect(
+    host = '127.0.0.1',
+    database = 'danil_kononov_knowledge_base',
+    user = 'danil_kononov_knowledge_base',
+    password = '1234'
+		)
+    cur = conn.cursor(cursor_factory=RealDictCursor)
     
+    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    user = cur.fetchone()
+    
+    if not user:
+        cur.close
+        conn.close()
+        return render_template('lab5/login.html', error = 'Логин и/или пороль неверны!')
+    if user['password'] != password:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html',error = 'Логин и/или пороль неверны!')
+    
+    session['login'] = login
+    cur.close()
+    conn.close()
+    return render_template('lab5/login_success.html', login=login)
+
